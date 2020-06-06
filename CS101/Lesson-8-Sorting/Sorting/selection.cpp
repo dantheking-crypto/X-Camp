a8f54fa24bef039e33a8ed287171237d548c1474
dantheking-crypto <danielfangzhu@gmail.com>
2020-06-06 15:49:40 -0700

    Bubblesort and selection sort

---
+++ CS101/Lesson-8-Sorting/Sorting/selection.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+inline void swaptwo (int arr[], int first, int second) {
+  int temp = arr[first];
+  arr[first] = arr[second];
+  arr[second] = temp;
+}
+void selectionsort(int arr[], int length) {
+    for (int i = 0; i < length - 2; i++) {
+        int least = i;
+        for (int j = i + 1; j < length - 1; j++) {
+            if (arr[j] < arr[least]) least = j;
+        }
+        swaptwo(arr, i, least);
+    }
+
+}
+int main() {
+    int length;
+    cin >> length;
+    int arr[length];
+    for (int i = 0; i < length; i++) {
+        cin >> arr[i];
+    }
+    selectionsort(arr, length);
+    for (int i = 0; i < length; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
