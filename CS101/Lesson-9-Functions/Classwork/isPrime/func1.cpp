8f10c95f727a1b42fef41a1a45251e202b9fab22
dantheking-crypto <danielfangzhu@gmail.com>
2020-06-13 14:50:11 -0700

    Create func1.cpp

---
+++ CS101/Lesson-9-Functions/Classwork/isPrime/func1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+bool isPrime (int query) {
+  if (query < 2) {
+    return false;
+  }
+  for (int i = 2; i < sqrt(query); i++) {
+    if (query % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+int main() {
+  int number;
+  cin >> number;
+  cout << boolalpha << isPrime (number);
+  return 0;
+}
