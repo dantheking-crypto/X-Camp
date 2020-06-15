cc94e48fde385463a4cec3beb486eef192c66c07
dantheking-crypto <danielfangzhu@gmail.com>
2020-06-15 12:02:39 -0700

    Create polynomial.cpp

---
+++ CS101/Lesson-9-Functions/Homework/polynomial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+inline int gausssum(int n) {
+  return (n * (n + 1)) / 2;
+}
+int main() {
+    int n;
+    cin >> n;
+    int total = 0;
+    for (int i = 1; i <= n; i++) {
+      total += gausssum(i);
+    }
+    cout << total << endl;
+    return 0;
+}
