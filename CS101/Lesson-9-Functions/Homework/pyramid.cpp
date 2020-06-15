07ff4e5530fa3f2ce859056f394d40eaa5f77861
dantheking-crypto <danielfangzhu@gmail.com>
2020-06-15 11:56:51 -0700

    Create pyramid.cpp

---
+++ CS101/Lesson-9-Functions/Homework/pyramid.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+int main() {
+  int pyramidlen;
+  while (cin >> pyramidlen) {
+    int starcount = 1;
+    int spacecount = pyramidlen - 1;
+    bool descending = false;
+    for (int i = 0; i < 2 * pyramidlen - 1; i++) {
+      for (int j = 0; j < spacecount; j++) {
+        cout << " ";
+      }
+      for (int j = 0; j < starcount; j++) {
+        cout << "*";
+      }
+      if (spacecount > 0 && !descending) {
+        spacecount--;
+      }
+      else {
+        spacecount++;
+        descending = true;
+      }
+       if (starcount < 2 * pyramidlen - 1 && !descending) {
+        starcount += 2;
+      }
+      else {
+        starcount -= 2;
+        descending = true;
+      }
+      cout << endl;
+    }
+  }
+  return 0;
+}
