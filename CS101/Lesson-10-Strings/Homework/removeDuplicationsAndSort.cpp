27e30d951e91b9e17767dbc159a23f109a153d67
dantheking-crypto <danielfangzhu@gmail.com>
2020-06-20 17:36:29 -0700

    Create removeDuplicationsAndSort.cpp

---
+++ CS101/Lesson-10-Strings/Homework/removeDuplicationsAndSort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+void mergetwosorted(int[], int[], int, int, int[]);
+void mergesort(int[], int);
+ 
+int main() {
+  int length;
+  cin >> length;
+  int arr[length];
+  for (int i = 0; i < length; i++) {
+    cin >> arr[i];
+  }
+  mergesort(arr, length);
+  int prev = arr[0] - 1;
+  for (int i = 0; i < length; i++) {
+    if (arr[i] != prev) cout << arr[i] << " ";
+    prev = arr[i];
+  }
+  return 0;
+}
+void mergetwosorted(int arr1[], int arr2[], int len1, int len2, int newarr[]) {
+   
+  int aindex = 0;
+  int bindex = 0;
+  int least1 = arr1[aindex];
+  int least2 = arr2[bindex];
+  for (int i = 0; i < len1 + len2; i++) {
+    if (least1 < least2) {
+      newarr[i] = least1;
+      if (aindex < len1 - 1) {
+        aindex++;
+        least1 = arr1[aindex];
+      }
+      else least1 = arr2[len2 - 1];
+    }
+    else {
+      newarr[i] = least2;
+      if (bindex < len2 - 1) {
+        bindex++;
+        least2 = arr2[bindex];
+      }
+      else least2 = arr1[len1 - 1];
+    }
+  }
+}
+void mergesort(int arr[], int length) {
+  if (length == 1) return;
+  int arr1[length / 2];
+  for (int i = 0; i < length / 2; i++) {
+    arr1[i] = arr[i];
+  }
+  int arr2[length - length / 2];
+  for (int i = length / 2; i < length; i++) {
+    arr2[i - length / 2] = arr[i];
+  }
+  mergesort(arr1, length / 2);
+  mergesort(arr2, length - length / 2);
+  mergetwosorted(arr1, arr2, length / 2, length - length / 2, arr);
+}
