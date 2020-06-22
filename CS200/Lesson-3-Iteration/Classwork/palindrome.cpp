3f70188d7a9875cbe454e2f908e1375d268a221f
dantheking-crypto <danielfangzhu@gmail.com>
2020-06-22 15:02:45 -0700

    Update palindrome.cpp
    Accounts for spaces within the palindrome.

--- CS200/Lesson-3-Iteration/Classwork/palindrome.cpp
+++ CS200/Lesson-3-Iteration/Classwork/palindrome.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
+bool isPalindrome (string myString, int i, int j) {
+  if (i >= j) return true;
+  char left = myString[i];
+  char right = myString[j];
+  if (left == ' ') {
+    i += 1;
+    left = myString[i];
+  }
+  if (right == ' ') {
+    j -= 1;
+    right = myString[j];
+  }
+  if (left != right) return false;
+  else return (isPalindrome(myString, i + 1, j - 1));
+}
 int main() {
   string myString;
-  cin >> myString;
-  bool isPalindrome = true;
-  for (int i = 0; i < myString.length() / 2.0; i++) {
-    if (myString[i] != myString[myString.length() - i - 1]) {
-      cout << "NO" << endl;
-      isPalindrome = false;
-      break;
-    }
-  }
-  if (isPalindrome) cout << "YES" << endl;
+  getline (cin, myString);
+  cout << boolalpha << isPalindrome(myString, 0, myString.length() - 1);
   return 0;
 }
